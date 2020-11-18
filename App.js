1eb127475a84ea9fb94a40605edb6abc7ef2d6ea
hayhaydz <haydon1900@gmail.com>
2020-11-18 13:02:22 +0000

    Completed Setup and Result Bar layout

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { AppLoading } from 'expo';
+import Constants from 'expo-constants';
+import {
+  useFonts,
+  PlayfairDisplay_400Regular,
+  PlayfairDisplay_700Bold,
+} from '@expo-google-fonts/playfair-display';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+  let [fontsLoaded] = useFonts({
+    PlayfairDisplay_400Regular,
+    PlayfairDisplay_700Bold,
+  });
+
+  if(fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.resultContainer}>
+          <Text style={styles.result}>9234</Text>
+        </View>
+        <View style={styles.unitContainer}>
+          
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else {
+    return <AppLoading />;
+  }
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#fff',
+    paddingTop: Constants.statusBarHeight,
+    margin: 16,
+  },
+  resultContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: '#FBFBFB',
+    maxHeight: 73,
+    minWidth: '100%',
+    borderColor: '#F4F4F4',
+    borderWidth: 3
+  },
+  result: {
+    fontFamily: 'PlayfairDisplay_700Bold',
+    fontSize: 32,
+    letterSpacing: 30
+  },
+  unitContainer: {
+
   },
 });
---
+++ components/UnitHandler/UnitHandler.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const UnitHandler = () => {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+export default UnitHandler;
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+})
