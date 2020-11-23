31ee8fd71195e66ace57a0de1d956131a0368d75
hayhaydz <haydon1900@gmail.com>
2020-11-23 15:47:31 +0000

    Working on UnitHandler

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { AppLoading } from 'expo';
 import Constants from 'expo-constants';
@@ -9,23 +9,36 @@ import {
   PlayfairDisplay_700Bold,
 } from '@expo-google-fonts/playfair-display';
 
+import UnitHandler from './components/UnitHandler/UnitHandler.js';
+
 const App = () => {
   let [fontsLoaded] = useFonts({
     PlayfairDisplay_400Regular,
     PlayfairDisplay_700Bold,
   });
 
+  let [ wholeValue, setWholeValue ] = useState(0);
+  let [ thousands, setThousands ] = useState([]);
+  let [ hundreds, setHundreds ] = useState([]);
+  let [ tens, setTens ] = useState([]);
+  let [ ones, setOnes ] = useState([]);
+
   if(fontsLoaded) {
     return (
-      <View style={styles.container}>
-        <View style={styles.resultContainer}>
-          <Text style={styles.result}>9234</Text>
-        </View>
-        <View style={styles.unitContainer}>
-          
-        </View>
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <View style={styles.resultContainer}>
+            <Text style={styles.result}>{wholeValue}</Text>
+          </View>
+          <View style={styles.unitContainer}>
+            <UnitHandler title="thousands" unit={thousands} setUnit={setThousands} wholeValue={wholeValue} setWholeValue={setWholeValue}/>
+            <UnitHandler title="hundreds" unit={hundreds} setUnit={setHundreds} wholeValue={wholeValue} setWholeValue={setWholeValue}/>
+            <UnitHandler title="tens" unit={tens} setUnit={setTens} wholeValue={wholeValue} setWholeValue={setWholeValue}/>
+            <UnitHandler title="ones" unit={ones} setUnit={setOnes} wholeValue={wholeValue} setWholeValue={setWholeValue}/>
+          </View>
+          <StatusBar style="auto" />
+        </ScrollView>
+      </SafeAreaView>
     );
   } else {
     return <AppLoading />;
@@ -48,17 +61,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-end',
     backgroundColor: '#FBFBFB',
-    maxHeight: 73,
+    minHeight: 73,
     minWidth: '100%',
     borderColor: '#F4F4F4',
-    borderWidth: 3
+    borderWidth: 3,
+    marginBottom: 25
   },
   result: {
     fontFamily: 'PlayfairDisplay_700Bold',
     fontSize: 32,
-    letterSpacing: 30
+    letterSpacing: 30,
   },
   unitContainer: {
-
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    minHeight: '100%'
   },
 });
