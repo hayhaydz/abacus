544922ce321df7ac8ec8723f5d25ddf419f16985
hayhaydz <haydon1900@gmail.com>
2020-12-04 14:03:40 +0000

    Information Modal Created. Splash screen and Icon created and added

--- App.js
+++ App.js
@@ -1,36 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
+import { 
+StyleSheet, 
+Text, 
+View, 
+SafeAreaView, 
+ScrollView, 
+Modal, 
+TouchableOpacity,
+Image,
+} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { AppLoading } from 'expo';
 import Constants from 'expo-constants';
 import { Html5Entities } from 'html-entities';
+import { Icon } from "react-native-elements";
 import {
   useFonts,
   PlayfairDisplay_400Regular,
   PlayfairDisplay_700Bold,
 } from '@expo-google-fonts/playfair-display';
+import {
+  Roboto_700Bold_Italic,
+} from '@expo-google-fonts/roboto';
 
 import UnitHandler from './components/UnitHandler/UnitHandler.js';
+import Logo from './assets/logo.png';
 
 const App = () => {
   let [fontsLoaded] = useFonts({
     PlayfairDisplay_400Regular,
     PlayfairDisplay_700Bold,
+    Roboto_700Bold_Italic,
   });
 
-  let [ wholeValue, setWholeValue ] = useState(0);
-  let [ romanValue, setRomanValue ] = useState("");
-  let [ thousands, setThousands ] = useState([]);
-  let [ hundreds, setHundreds ] = useState([]);
-  let [ tens, setTens ] = useState([]);
-  let [ ones, setOnes ] = useState([]);
+  const [ wholeValue, setWholeValue ] = useState(0);
+  const [ romanValue, setRomanValue ] = useState("");
+  const [ thousands, setThousands ] = useState([]);
+  const [ hundreds, setHundreds ] = useState([]);
+  const [ tens, setTens ] = useState([]);
+  const [ ones, setOnes ] = useState([]);
+
+  const [ modalVisible, setModalVisible ] = useState(false);
 
   const arabicArray = [5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  const romanArray = ['V\&#773;', 'MV\&#773', 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  const romanArray = ['V\&#773;', 'MV\&#773;', 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
   const entities = new Html5Entities();
 
   useEffect(() => {
-    console.log(wholeValue);
     // https://www.freecodecamp.org/news/roman-numeral-converter-interactive-roman-numerals-chart/
     let arabic = wholeValue;
     let roman = '';
@@ -47,13 +63,35 @@ const App = () => {
 
       setRomanValue(roman);
     }
-    console.log(romanValue);
   });
 
   if(fontsLoaded) {
     return (
       <SafeAreaView style={styles.container}>
         <ScrollView>
+          <Modal
+            animationType="fade"
+            visible={modalVisible}
+            transparent={true}
+          >
+            <View style={styles.modalContainer}>
+              <Image source={Logo} style={styles.modalLogo} />
+              <Text style={styles.modalHeading}>How to use the abacus</Text>
+              <Text style={styles.modalText}>Using the plus (+) and minus (-) buttons underneath the  respected unit value will result in that value changing by one. This is visually demonstrated by a bead being added to it’s pole.</Text>
+              <Text style={[styles.modalText, {marginBottom: 50}]}>Once a bead is added it’s roman numeral conversion will be displayed above. Add multiple beads to multiple units to see the roman numeral number increase.</Text>
+              <Text style={styles.modalHeading}>Additional Information</Text>
+              <Text style={[styles.modalText, {fontFamily: 'PlayfairDisplay_700Bold', marginBottom: 5}]}>Created By</Text>
+              <Text style={styles.modalText}>Haydon Curteis-Lateo</Text>
+              <Text style={[styles.modalText, {fontFamily: 'PlayfairDisplay_700Bold', marginBottom: 5}]}>Contact Information</Text>
+              <Text style={styles.modalText}>haydon.curteis-lateo18@bathspa.ac.uk</Text>
+              <TouchableOpacity
+                onPress={() => setModalVisible(!modalVisible)}
+                style={styles.modalClose}
+              >
+                <Icon name="cancel" type="material" size={32}></Icon>
+              </TouchableOpacity>
+            </View>
+          </Modal>
           <View style={styles.resultContainer}>
             {/* https://stackoverflow.com/questions/56558371/render-unicode-characters-in-react-native-using-variable/56560732 */}
             <Text style={styles.result}>{entities.decode(romanValue)}</Text>
@@ -63,6 +101,12 @@ const App = () => {
             <UnitHandler title="hundreds" unit={hundreds} setUnit={setHundreds} wholeValue={wholeValue} setWholeValue={setWholeValue} colour="#0C8120" />
             <UnitHandler title="tens" unit={tens} setUnit={setTens} wholeValue={wholeValue} setWholeValue={setWholeValue} colour="#3C61ED" />
             <UnitHandler title="ones" unit={ones} setUnit={setOnes} wholeValue={wholeValue} setWholeValue={setWholeValue} colour="#FF6B00" />
+            <TouchableOpacity
+              onPress={() => setModalVisible(true)}
+              style={{padding: 15}}
+            >
+              <Icon name="info-circle" type="font-awesome-5" size={32} ></Icon>
+            </TouchableOpacity>
           </View>
           <StatusBar style="auto" />
         </ScrollView>
@@ -93,17 +137,47 @@ const styles = StyleSheet.create({
     minWidth: '100%',
     borderColor: '#F4F4F4',
     borderWidth: 3,
-    marginBottom: 25
+    marginBottom: 25,
+    paddingRight: 25,
   },
   result: {
     fontFamily: 'PlayfairDisplay_700Bold',
     fontSize: 32,
-    // letterSpacing: 30,
   },
   unitContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start',
-    minHeight: '100%'
+    minHeight: '90%',
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    padding: 16,
+    backgroundColor: 'rgba(255,255,255,0.98)',
+  },
+  modalClose: {
+    position: 'absolute',
+    top: 12,
+    right: 1,
+    padding: 15
+  },
+  modalLogo: {
+    marginBottom: 50,
+  },
+  modalHeading: {
+    fontFamily: 'Roboto_700Bold_Italic',
+    fontSize: 32,
+    maxWidth: '75%',
+    marginBottom: 25,
+    color: '#1C1C1C',
+  },
+  modalText: {
+    fontFamily: 'PlayfairDisplay_400Regular',
+    fontSize: 16,
+    lineHeight: 28,
+    marginBottom: 15,
+    color: '#252525',
   },
 });
--- components/UnitHandler/UnitHandler.js
+++ components/UnitHandler/UnitHandler.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { 
+StyleSheet, 
+Text, 
+View, 
+TouchableOpacity 
+} from "react-native";
 import { Icon } from "react-native-elements";
 
 const UnitHandler = ({ title, unit, setUnit, wholeValue, setWholeValue, colour }) => {
